4489dbf06311dd42c92f501a21219c1b4b64c183
harish.d@dvara.com <harish.d14@iiits.in>
2020-01-07 11:58:52 +0530

    added support for direct and indirect

--- app/content.js
+++ app/content.js
@@ -2,14 +2,46 @@ window.addEventListener('contextmenu', e => {
     if (e.target.className != 'breadcrumb' && e.target.parentElement.className != 'breadcrumb' && e.target.parentElement.parentElement.className != 'breadcrumb') {
         chrome.runtime.onMessage.addListener(mylistner)
         function mylistner(request, sender, sendResponse) {
-            var p = (((request.text.split('_').map(x => x.split(' '))) + '').split(',').map(x => (x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())) + '').replace(/,/g, ' ')
-            var text = e.target.innerHTML || '';  
-            if (text.includes(request.text)){
-                text = text.replace(request.text,p);
-                e.target.innerHTML = text;
-            }
+            chrome.storage.local.get(['translations'],function(value){
+                translations = {};
+                if(value.translations){
+                    translations = value.translations;
+                }
+                if(e.target.getAttribute('ac-tran')){
+                    var code = e.target.getAttribute('ac-tran');
+                    var value = (((code.split('_').map(x => x.split(' '))) + '').split(',').map(x => (x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())) + '').replace(/,/g, ' ')
+                    translations[code] = {
+                        code :code,
+                        value:value,
+                        direct:true
+                    }
+                }
+                else{
+                    var direct = false;
+                    if(!e.target.innerHTML)
+                        code = e.target.value;
+                    else 
+                        code = e.target.innerHTML;
+                    if(code.includes('_')){
+                        direct = true;   
+                    }
+                    var code = code;
+                    var value = (((code.split('_').map(x => x.split(' '))) + '').split(',').map(x => (x.charAt(0).toUpperCase() + x.slice(1).toLowerCase())) + '').replace(/,/g, ' ');
+                    translations[code] = {
+                        code :code,
+                        value:value,
+                        direct:direct
+                    }
+                    if(!e.target.innerHTML){
+                        e.target.value = value;
+                    }
+                    else{
+                        e.target.innerHTML  = value;
+                    }
+                }
+                chrome.storage.local.set({'translations':translations},function(){});
+            })
             chrome.runtime.onMessage.removeListener(mylistner);
         }
-
     }
 })
--- eventPage.js
+++ eventPage.js
@@ -6,39 +6,11 @@ var menuItem = {
 chrome.contextMenus.create(menuItem);
 chrome.contextMenus.onClicked.addListener(function(clickData){  
     if (clickData.menuItemId == "irf-translate" && clickData.selectionText){    
-        chrome.storage.local.get(['translations'],function(value){
-            if (typeof value.translations != "undefined"){
-                var temp_trans = value.translations;
-                debugger;
-                var code = clickData.selectionText;
-                if(typeof temp_trans[code] != 'undefined'){
-                    if (temp_trans[code].weight != 1 && temp_trans[code].session != 1){
-                        temp_trans[code].code = code;
-                        temp_trans[code].session = 1;
-                    }
-                }
-                else{
-                    temp_trans[code] = {};
-                    temp_trans[code].code = code;
-                    temp_trans[code].session = 1;
-                }
-                chrome.storage.local.set({'translations':temp_trans},function(){});
-            }
-            else{
-                var temp_trans = {};
-                temp_trans[clickData.selectionText] ={
-                        'code':clickData.selectionText,
-                        'weight':0,
-                        'session':1,
-                }
-                chrome.storage.local.set({'translations':temp_trans},function(){});
-            }
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {text: clickData.selectionText}, function(response) {
-                //   console.log(response.farewell);
-                });
-              });
-        })
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {text: clickData.selectionText}, function(response) {
+            //   console.log(response.farewell);
+            });
+          });
     }
 });
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
